f8179edb286c015e099307ebe80eb8e0091c0e51
Laboratorio 08 <laboratoriounam-apple08@macbook-pro-de-laboratorio-8.local>
2019-03-06 14:57:00 -0600

    UPTADE: Creando una vista de manera programática

--- pruebaGitHub/ViewController.swift
+++ pruebaGitHub/ViewController.swift
@@ -12,12 +12,40 @@ class ViewController: UIViewController {
 
     @IBAction func ButtonPressed(_ sender: UIButton) {
     }
+   
+    //override: permite agregar funcionalidad a la función
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        /* Top -> Arriba
+           Bottom -> Abajo
+           Leading -> Izquierda
+           Trailing -> Derecha
+         */
+        
+        let viewRoja = UIView()
+        
+        viewRoja.backgroundColor = .red
+        
+        view.addSubview(viewRoja)
+    viewRoja.translatesAutoresizingMaskIntoConstraints = false
+        
+        viewRoja.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        viewRoja.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        viewRoja.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        viewRoja.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        
     }
-
-
 }
 
-
+extension UIView {
+    func anchor(top: NSLayoutYAxisAnchor, leading: NSLayoutXAxisAnchor, trailing: NSLayoutXAxisAnchor, bottom: NSLayoutYAxisAnchor){
+       
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        topAnchor.constraint(equalTo: top).isActive = true
+       leadingAnchor.constraint(equalTo: leading).isActive = true
+        trailingAnchor.constraint(equalTo: trailing).isActive = true
+        bottomAnchor.constraint(equalTo: bottom).isActive = true
+    }
+}
