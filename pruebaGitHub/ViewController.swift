be544e6766e4e69cf5f453dff2d5e73662c29e15
Laboratorio 08 <laboratoriounam-apple08@macbook-pro-de-laboratorio-8.local>
2019-03-06 13:59:08 -0600

    ADD: Se agrega button y su action

--- pruebaGitHub/ViewController.swift
+++ pruebaGitHub/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func ButtonPressed(_ sender: UIButton) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -17,4 +19,5 @@ class ViewController: UIViewController {
 
 
 }
+
 
